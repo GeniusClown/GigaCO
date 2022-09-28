35a434388a3759301422175654c43f81d11ec760
303-9 <303-9@example.com>
2022-09-28 11:45:49 +0300

    sad

--- GIgaCo/siz.xaml.cs
+++ GIgaCo/siz.xaml.cs
@@ -24,12 +24,12 @@ namespace GIgaCo
         public siz()
         {
             InitializeComponent();
-            pdfWebViewer.Navigate(new Uri("https://compsch.com/obzor/texnika-bezopasnosti-pri-rabote-s-kompyuterom.html"));
+            
         }
 
         private void silka(object sender, RoutedEventArgs e)
         {
-            Process.Start("https://app.roboflow.com/farmmoney/cars-wvxqu/1");
+            Process.Start("https://app.roboflow.com/project/gloves-fnjh9/1");
         }
     }
 }
